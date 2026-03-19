b77d1b22212228a0cbe8874f1f0004663ebe7299
Aphisit <Apisit@origami.life>
2026-03-19 10:03:18 +0700

    แก้ไขการจัดการ

--- app/admin/check_permission.php
+++ app/admin/check_permission.php
@@ -1,4 +1,9 @@
 <?php
+// ป้องกัน browser cache หน้า admin
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
+
 require_once(__DIR__ . '/../../lib/connect.php');
 require_once(__DIR__ . '/../../lib/base_directory.php');
 require_once(__DIR__ . '/../../lib/permissions.php');
